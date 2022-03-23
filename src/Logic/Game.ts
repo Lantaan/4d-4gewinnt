d2aef3f5b17193bded741901ac68366ab9ff0945
DerkomischeTyp <paul@qwhon.de>
2022-03-23 16:10:52 +0100

    game everything but without winnning conditions

--- src/Logic/Game.ts
+++ src/Logic/Game.ts
@@ -1,20 +1,31 @@
 import React from "react";
-import Tutorial from "./Tutorial";
 class Game{
-    board: number[][][][]=[[[[ ]]]];
+    activeplayer: ("player1" | "player2") = "player1";
+    board: ("no marker"|"player1" |"player2") [][][][]=[[[[]]]];
     constructor(){
-        let yay: Tutorial = new Tutorial()
+        //am Anfang sind alle Felder leer
         for (let a: number =0; a<4; a++) {
             for (let b: number =0; b<4; b++) {
                 for (let c: number =0; c<4; c++) {
                     for (let d: number =0; d<4; d++) {
                         this.board[a]=[];
                         this.board[a][b]=[];
-                        this.board[a][b][c][d]=0;
+                        this.board[a][b][c]=[];
+                        this.board[a][b][c][d]="no marker";
                     }
                 }
             }
         }
+    }
+    turn(x:number, y:number, z:number, w:number): string{
+        if(this.board[x][y][z][w]!= "no marker"){return "Das Feld ist schon voll"}
+        this.board[x][y][z][w]=this.activeplayer;
+        if(this.activeplayer="player1"){this.activeplayer="player2"}else{this.activeplayer="player1"}
+        if(this.checkcompleterow() == true){return "congratulations " + this.activeplayer + " won"}
+        return this.activeplayer + ", it's your turn!";
+    }
+    checkcompleterow():boolean{
+        return false;
     }
 }
 export default Game
