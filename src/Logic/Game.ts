9b6ac5c646a9ef488ae9606205a31684f34ecc7f
DerkomischeTyp <paul@qwhon.de>
2022-03-23 14:54:09 +0100

    besseres board

--- src/Logic/Game.ts
+++ src/Logic/Game.ts
@@ -4,7 +4,17 @@ class Game{
     board: number[][][][]=[[[[]]]];
     constructor(){
         let yay: Tutorial = new Tutorial()
-
+        for (let a: number =0; a<4; a++) {
+            for (let b: number =0; b<4; b++) {
+                for (let c: number =0; c<4; c++) {
+                    for (let d: number =0; d<4; d++) {
+                        this.board[a]=[];
+                        this.board[a][b]=[];
+                        this.board[a][b][c][d]=0;
+                    }
+                }
+            }
+        }
     }
 }
 export default Game
