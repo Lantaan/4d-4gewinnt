22f7eced8e4714a93074e9f5cbacdba1d14d315d
Lantaan <69048952+Lantaan@users.noreply.github.com>
2022-05-08 15:07:00 +0200

    fully fleshed out popup functionality

--- src/UI/Plane.tsx
+++ src/UI/Plane.tsx
@@ -1,39 +1,33 @@
-import { Color, Vector3, Vector4 } from "three";
-import Game from "../Logic/Game";
-import { useMouseRaycaster } from "./Raycaster";
-import Tile from "./Tile";
+import { Color, Vector3, Vector4 } from 'three';
+import Game from '../Logic/Game';
+import Tile from './Tile';
 
+function Plane(props: { displayPos: Vector3; wPos: number; yPos: number; gameObject: Game }) {
+	const relativePositionArray: Vector3[] = [];
+	for (let x = 0; x < 4; x++) {
+		for (let z = 0; z < 4; z++) {
+			relativePositionArray.push(new Vector3(x, 0, z));
+		}
+	}
 
-function Plane(props: {
-    displayPos: Vector3,
-    wPos: number, yPos: number,
-    gameObject: Game,
-}) {
+	return (
+		<>
+			{relativePositionArray.map((pos, i) => {
+				const gamePos = new Vector4(pos.x, props.yPos, pos.z, props.wPos),
+					filling = props.gameObject.board[gamePos.x][gamePos.y][gamePos.z][gamePos.w];
 
-    useMouseRaycaster();
-
-    const relativePositionArray: Vector3[] = [];
-    for (let x = 0; x < 4; x++) {
-        for (let z = 0; z < 4; z++) {
-            relativePositionArray.push(new Vector3(x, 0, z));
-        }
-    }
-
-    return <>{
-        relativePositionArray.map((pos, i) => {
-            const gamePos = new Vector4(pos.x, props.yPos, pos.z, props.wPos),
-                filling = props.gameObject.board[gamePos.x][gamePos.y][gamePos.z][gamePos.w];
-
-            return <Tile
-                gameObject={props.gameObject}
-                gamePos={gamePos}
-                displayPos={pos.add(props.displayPos)}
-                filledBy={filling}
-                key={i}
-            />
-        })
-    }</>
+				return (
+					<Tile
+						gameObject={props.gameObject}
+						gamePos={gamePos}
+						displayPos={pos.add(props.displayPos)}
+						filledBy={filling}
+						key={i}
+					/>
+				);
+			})}
+		</>
+	);
 }
-
 
 export default Plane;
--- src/UI/Popup.tsx
+++ src/UI/Popup.tsx
@@ -1,33 +1,37 @@
-/*class Popup {
-	constructor(private content: HTMLElement | string) {}
-	show() {
-		const container = document.createElement('div');
-		container.classList.add('popup-container');
-
-		if (this.content instanceof HTMLElement) container.appendChild(this.content);
-        else container.innerHTML = this.content;
-
-		document.body.appendChild(container);
-	}
-}*/
-
 import { ReactElement, useState } from 'react';
 
 let popupContent: ReactElement | null = null,
-	setPopupContent: React.Dispatch<React.SetStateAction<ReactElement|null>>|undefined;
+	setPopupContent: React.Dispatch<React.SetStateAction<ReactElement | null>> | undefined;
 
 function PopupComponent(props: {}) {
-	[popupContent, setPopupContent] = useState<ReactElement|null>(null);
-	return <div className={popupContent ? 'popup-container' : ''}>{popupContent}</div>;
+	[popupContent, setPopupContent] = useState<ReactElement | null>(null);
+
+	if (popupContent) {
+		return (
+			<>
+				<div className='popup-bg' onClick={()=>openPopup?.close()}></div>
+				<div className='popup-container'>{popupContent}</div>
+			</>
+		);
+	}
+
+	return <></>;
 }
 
+let openPopup: Popup|null = null;
 class Popup {
 	constructor(private content: ReactElement) {}
 	show() {
-		if (setPopupContent) setPopupContent(this.content);
+		if (setPopupContent) {
+			setPopupContent(this.content);
+			openPopup = this;
+		}
 	}
 	close() {
-		if (setPopupContent) setPopupContent(null);
+		if (setPopupContent) {
+			setPopupContent(null);
+			openPopup = null;
+		}
 	}
 }
 
