66dd35e315e49df8c4ae0e19063c9684e09872d9
Lantaan <69048952+Lantaan@users.noreply.github.com>
2022-06-16 19:28:40 +0200

    bug fixes

--- src/Logic/Game.ts
+++ src/Logic/Game.ts
@@ -1,82 +1,99 @@
-import React from "react";
+import React from 'react';
 
-import { Vector4 } from "three";
+import { Vector4 } from 'three';
 
 class Game {
-    activeplayer: ("player1" | "player2") = "player1";
-    board: ("no marker" | "player1" | "player2")[][][][] = [[[[]]]];
-    constructor() {
-        //am Anfang sind alle Felder leer
+	activeplayer: 'player1' | 'player2' = 'player1';
+	board: ('no marker' | 'player1' | 'player2')[][][][] = [[[[]]]];
+	constructor() {
+		//am Anfang sind alle Felder leer
 
-        for (let a: number =0; a<4; a++) {
-            this.board[a]=[];
-            for (let b: number =0; b<4; b++) {
-                this.board[a][b]=[];
-                for (let c: number =0; c<4; c++) {
-                    this.board[a][b][c]=[];
-                    for (let d: number =0; d<4; d++) {
-                        this.board[a][b][c][d]="no marker";
-                    }
-                }
-            }
-        }
-    }
+		for (let a: number = 0; a < 4; a++) {
+			this.board[a] = [];
+			for (let b: number = 0; b < 4; b++) {
+				this.board[a][b] = [];
+				for (let c: number = 0; c < 4; c++) {
+					this.board[a][b][c] = [];
+					for (let d: number = 0; d < 4; d++) {
+						this.board[a][b][c][d] = 'no marker';
+					}
+				}
+			}
+		}
+	}
 
-    turn(pos: Vector4): {winner: ("player1"|"player2"|null); turn: ("player1"|"player2"|null)}{
-        //Feld ist schon besetzt
-        if(this.board[pos.x][pos.y][pos.z][pos.w]!= "no marker"){
-            return {winner: null, turn: null}
-        }
-        //Einer der Spieler hat eine Reihe voll
-        this.board[pos.x][pos.y][pos.z][pos.w]=this.activeplayer;
-        if(this.checkifcompleterow(pos)){return {winner: this.activeplayer, turn: null}}
-        //Das Spiel geht normal weiter
-        if(this.activeplayer=="player1"){this.activeplayer="player2"}else{this.activeplayer="player1"}
-        return {winner: null,turn: this.activeplayer}
-    }
+	turn(pos: Vector4): { winner: 'player1' | 'player2' | null; turn: 'player1' | 'player2' | null } {
+		//Feld ist schon besetzt
+		if (this.board[pos.x][pos.y][pos.z][pos.w] != 'no marker') {
+			return { winner: null, turn: null };
+		}
+		//Einer der Spieler hat eine Reihe voll
+		this.board[pos.x][pos.y][pos.z][pos.w] = this.activeplayer;
+		if (this.checkifcompleterow(pos)) {
+			return { winner: this.activeplayer, turn: null };
+		}
+		//Das Spiel geht normal weiter
+		if (this.activeplayer == 'player1') {
+			this.activeplayer = 'player2';
+		} else {
+			this.activeplayer = 'player1';
+		}
+		return { winner: null, turn: this.activeplayer };
+	}
 
-    checkifcompleterow(pos:Vector4): boolean {
-        let dir = new Vector4(0,0,0,0);
-        //checkrow wird mit allen verschiedenen Richtungen durchgeführt
-for (let i=-1; i<2; i++){
-    dir.setX(i);
-    for (let j=-1; j<2; j++){
-        dir.setY(j);
-        for (let k=-1; k<2; k++){
-            dir.setZ(k);
-            for (let l=-1; l<2;l++)
-            dir.setW(l);
-            if (dir.x==dir.y&&dir.z==dir.w&&dir.w==dir.x&&dir.x==0){}
-            else{
-            if (this.checkrow(pos,dir))return true;
-            }
-        } 
-    }
+	checkifcompleterow(pos: Vector4): boolean {
+		let dir = new Vector4(0, 0, 0, 0);
+		//checkrow wird mit allen verschiedenen Richtungen durchgeführt
+		for (let i = -1; i < 2; i++) {
+			dir.setX(i);
+			for (let j = -1; j < 2; j++) {
+				dir.setY(j);
+				for (let k = -1; k < 2; k++) {
+					dir.setZ(k);
+					for (let l = -1; l < 2; l++) {
+						dir.setW(l);
+						if (dir.x == dir.y && dir.z == dir.w && dir.w == dir.x && dir.x == 0) {
+						} else {
+							if (this.checkrow(pos.clone(), dir.clone())) return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+	checkrow(pos: Vector4, direction: Vector4): boolean {
+		let contents: string[] = [];
+		while (pos.x < 4 && pos.y < 4 && pos.z < 4 && pos.w < 4 && pos.x > -1 && pos.y > -1 && pos.z > -1 && pos.w > -1) {
+			//die Reihe wird bis zu einem Ende durchgegangen
+			//dabei werden alle Inhalte der Reihe gespeichert
+			contents.push(this.board[pos.x][pos.y][pos.z][pos.w]);
+			pos.add(direction);
+		}
+		//durchgehen der Reihe in beide Richtungen
+		while (pos.x > -1 && pos.y > -1 && pos.z > -1 && pos.w > -1 && pos.x < 4 && pos.y < 4 && pos.z < 4 && pos.w < 4) {
+			contents.push(this.board[pos.x][pos.y][pos.z][pos.w]);
+			pos.sub(direction);
+		}
+		//Wenn einer der Spieler die Reihe voll hat, wird true zurückgegeben
+		if (
+			contents[0] == contents[3] &&
+			contents[1] == contents[2] &&
+			contents[1] == contents[0] &&
+			contents[0] != 'no marker'
+		) {
+			console.log(contents);
+			return true;
+		}
+		return false;
+	}
+	test() {
+		this.board[0][0][0][0] = 'player1';
+		this.board[1][0][1][0] = 'player1';
+		this.board[2][0][2][0] = 'player1';
+		this.board[3][0][3][0] = 'player1';
+		let pos = new Vector4(2, 0, 2, 0);
+		console.log(this.checkifcompleterow(pos));
+	}
 }
-        return false;
-    }
-    checkrow(pos: Vector4, direction: Vector4): boolean{
-        let contents: string[] = [];
-        while (pos.x<4 && pos.y<4 && pos.z<4 && pos.w<4){
-            //die Reihe wird bis zu einem Ende durchgegangen
-        pos = pos.add(direction);
-     }
-        while (pos.x>-1 && pos.y>-1 && pos.z>-1 && pos.w>-1){
-            //dabei werden alle Inhalte der Reihe gespeichert
-         contents.push(this.board[pos.x][pos.y][pos.z][pos.w])
-         pos = pos.sub(direction);   
-        }
-        //Wenn einer der Spieler die Reihe voll hat, wird true zurückgegeben
-        if(contents[0] == contents[3] && contents[1]==contents[2] && contents[1] == contents[0] && contents[0] != "no marker"){return true}
-        return false
-    }
-    test(){
-        this.board[0][0][0][0] = "player1";
-        this.board[1][0][1][0] = "player1";
-        this.board[2][0][2][0] = "player1";
-        this.board[3][0][3][0] = "player1";
-        let  pos = new Vector4(2,0,2,0);
-        console.log(this.checkifcompleterow(pos));
-   }
-    }  
-export default Game
+export default Game;
