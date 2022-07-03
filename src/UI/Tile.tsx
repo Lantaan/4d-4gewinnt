ade9c628dfea459fbff4058806be0faa08434ec7
Lantaan <andreas.iskrov@gmail.com>
2022-07-03 19:12:36 +0200

    documentation, code refactoring

--- src/UI/Tile.tsx
+++ src/UI/Tile.tsx
@@ -1,68 +1,103 @@
-import { useEffect, useRef, useState } from 'react';
-import { Color, Mesh, Vector3, Vector4 } from 'three';
-import Game from '../Logic/Game';
-import Box from './Box';
-import { Popup } from './Popup';
+import { useEffect, useRef, useState } from "react";
+import { Color, Mesh, Vector3, Vector4 } from "three";
+import Game from "../Logic/Game";
+import Box from "./Box";
+import { Popup } from "./Popup";
 
 function Tile(props: {
-	displayPos: Vector3;
-	gamePos: Vector4;
-	gameObject: Game;
-	filledBy: 'no marker' | 'player1' | 'player2';
+  displayPos: Vector3;
+  gamePos: Vector4;
+  gameObject: Game;
+  filledBy: "no marker" | "player1" | "player2";
 }) {
-	const [hover, setHover] = useState(false),
-		[filledBy, setFilledBy] = useState<'no marker' | 'player1' | 'player2'>(props.filledBy);
+  //wenn setHover oder setFilledBy aufgerufen wird, wird Tile rerendert
+  const [hover, setHover] = useState(false),
+    [filledBy, setFilledBy] = useState<"no marker" | "player1" | "player2">(
+      props.filledBy
+    );
 
-	const transparent = filledBy === 'no marker' && !hover;
+  //wenn man über den Würfel hovert, soll es gefüllt werden,
+  //also nicht durchsichtig sein
+  const transparent = filledBy === "no marker" && !hover;
 
-	let color: Color = new Color(0x000000);
-	if (filledBy === 'player1') color = new Color(0xff0000);
-	else if (filledBy === 'player2') color = new Color(0x00ff00);
-	else if (!filledBy) color = new Color(0x000000);
+  //füllfarbe vom Würfel
+  //wenn Würfel gefüllt ist, ist entweder rot oder grün
+  //wenn würfel nicht gefüllt ist, spielt color nur eine Rolle,
+  //wenn man drüberhovert und dann soll der Würfel mit schwarz gefüllt sein
+  let color: Color = new Color(0x000000);
+  if (filledBy === "player1") color = new Color(0xff0000);
+  else if (filledBy === "player2") color = new Color(0x00ff00);
+  else color = new Color(0x000000);
 
-	return (
-		<>
-			<Box
-				pos={props.displayPos.clone()}
-				color={color}
-				transparent={transparent}
-				onMouseEnter={() => setHover(true)}
-				onMouseLeave={() => setHover(false)}
-				onMouseDown={() => {
-					const gamePos = props.gamePos,
-						turnResult = props.gameObject.turn(props.gamePos);
+  return (
+    <>
+      <Box
+        //.clone() notwendig, damit pos nicht passed by reference wird
+        pos={props.displayPos.clone()}
+        color={color}
+        transparent={transparent}
+        //wenn Maus über dem Würfel ist, ist hover true, sonst false
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        //wenn auf Würfel draufgeclickt wird, soll:
+        //ein Zug gemacht werden
+        //ein Popup mit infos zum Zug angezeigt werden
+        //wenn ein Zug nicht gemacht werden kann (z.B. weil der Würfel schon gefüllt ist),
+        //handelt diese Situation trotzdem die turn methode
+        onMouseDown={() => {
+          const gamePos = props.gamePos,
+            turnResult = props.gameObject.turn(props.gamePos);
 
-					let turnMessage: string, messageColor: string;
-					if (!turnResult.winner && turnResult.turn) {
-						turnMessage = `It's your turn ${turnResult.turn}`;
-						messageColor = 'black';
-					} else if (turnResult.winner) {
-						turnMessage = `${turnResult.winner} won!`;
-						messageColor = 'green';
-					} else {
-						turnMessage = `This is not a valid placement`;
-						messageColor = 'red';
-					}console.log(turnResult);
+          //turnMessage und messageColor werden im Popup benutzt
+          let turnMessage: string, messageColor: string;
+          //wenn es keinen Sieger gibt, aber ein Zug gemacht werden konnte,
+          if (!turnResult.winner && turnResult.turn) {
+            //string interpolation ist fancy für:
+            //turnMessage = "It's your turn" + turnResult.turn.toString();
+            turnMessage = `It's your turn ${turnResult.turn}`;
+            messageColor = "black";
+          } else if (turnResult.winner) {
+            turnMessage = `${turnResult.winner} won!`;
+            messageColor = "green";
+          } else {
+            //wenn ein Zug nicht gemacht werden konnte
+            turnMessage = `This is not a valid placement`;
+            messageColor = "red";
+          }
 
-					const popup = new Popup(
-						(
-							<>
-								<h1 style={{ paddingLeft: '20px', paddingRight: '20px', color: messageColor }}>{turnMessage}</h1>
-								<div style={{ justifyContent: 'center', display: 'flex' }}>
-									<button onClick={() => popup.close()} style={{fontSize: "20px", width: "60px"}}>
-										OK
-									</button>
-								</div>
-							</>
-						)
-					);
-					popup.show();
+          const popup = new Popup(
+            (
+              <>
+                <h1
+                  style={{
+                    paddingLeft: "20px",
+                    paddingRight: "20px",
+                    color: messageColor,
+                  }}
+                >
+                  {turnMessage}
+                </h1>
+                <div style={{ justifyContent: "center", display: "flex" }}>
+                  <button
+                    onClick={() => popup.close()}
+                    style={{ fontSize: "20px", width: "60px" }}
+                  >
+                    OK
+                  </button>
+                </div>
+              </>
+            )
+          );
+          popup.show();
 
-					setFilledBy(props.gameObject.board[gamePos.x][gamePos.y][gamePos.z][gamePos.w]);
-				}}
-			/>
-		</>
-	);
+          //updatet Anzeigefarbe von Würfel
+          setFilledBy(
+            props.gameObject.board[gamePos.x][gamePos.y][gamePos.z][gamePos.w]
+          );
+        }}
+      />
+    </>
+  );
 }
 
 export default Tile;
--- src/UI/Tower.tsx
+++ src/UI/Tower.tsx
@@ -2,23 +2,39 @@ import { Vector3 } from "three";
 import Game from "../Logic/Game";
 import Plane from "./Plane";
 
+//props.displayPos, props.wPos und props.gameObject können in der Funktion aufgerufen werden
+//props.displayPos und props.wPos sind 3d Vektoren
+//props.gameObject ist eine Instanz von Game (in ../Logic/)
 function Tower(props: { displayPos: Vector3; wPos: number; gameObject: Game }) {
+  //Feld, in dem jedes element, ein objekt ist,
+  //und die attribute game, und display als 3d Vektoren hat
+  //speichert die positionen der einzelnen Ebenen in einem Turm
+  //Koordinaten werden relativ zu Turm Koordinaten angegeben
+  //relativePositionArray[0].game gibt die position der Ebene im props.gameObject (Logik-Teil) an
+  //relativePositionArray[0].display gibt die vordere linke Ecke
+  //der angezeigten Ebene an (relativ zu props.displayPos)
   const relativePositionArray: { game: Vector3; display: Vector3 }[] = [];
+  //füllt das Feld
   for (let y = 0; y < 4; y++) {
+    //Feld.push hängt element ganz hinten an
     relativePositionArray.push({
       game: new Vector3(0, y, 0),
-      display: new Vector3(0, y*3, 0).add(props.displayPos),
+      //in y-Richtung gestreckt, damit man auch die inneren Zellen anclicken kann
+      display: new Vector3(0, y * 3, 0).add(props.displayPos),
     });
   }
 
   return (
     <>
-      {relativePositionArray.map((pos, i) => (
+      {//für jedes element in relativePositionsArray
+      //erzeuge eine Ebene (bzw. führe Plane() aus)
+      relativePositionArray.map((pos, i) => (
         <Plane
           displayPos={pos.display.clone()}
           yPos={pos.game.y}
           wPos={props.wPos}
           gameObject={props.gameObject}
+          //key wird von React benutzt für interne Optimierung aka schwarze Magie
           key={i}
         />
       ))}
